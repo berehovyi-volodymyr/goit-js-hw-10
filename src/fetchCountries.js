77c642621ee7902dc0eea9d00f4694e73fa32079
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2022-11-30 19:47:13 +0200

    done hw

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,12 @@
-export function fetchCountries(name){
-    return fetch(
-  `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-)
-  .then(r => r.json())
-  .then( data  => data);
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {if(!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();})
+    .then(data => data);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 1000;
 
@@ -17,7 +18,25 @@ refs.input.addEventListener('input', debounce(selectCountries, DEBOUNCE_DELAY));
 function selectCountries(e) {
   e.preventDefault();
   value = refs.input.value.trim();
-  fetchCountries(value).then(countries => renderOneCountry(countries));
+  if (value === '') {
+    clearMarkup();
+    return;
+  }
+  fetchCountries(value)
+    .then(countries => {
+      if (countries.length === 1) {
+        clearMarkup();
+        renderOneCountry(countries);
+      } else if (countries.length >= 1 && countries.length <= 10) {
+        clearMarkup();
+        renderList(countries);
+      } else if (countries.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+    })
+    .catch(error, clearMarkup());
 }
 
 function renderList(countries) {
@@ -33,18 +52,28 @@ function renderList(countries) {
 }
 
 function renderOneCountry(countries) {
-    const markup = countries
+  const lang = countries.map(({ languages }) => Object.values(languages));
+  const markup = countries
     .map(
       country => `<div>
-  <div>
-    <img class ="flag" src="${country.flags.svg}" alt="flag" />
-    <p>${country.name.official}</p>
+  <div class ="header">
+    <img class ="big_flag" src="${country.flags.svg}" alt="flag" />
+    <p class="title">${country.name.official}</p>
   </div>
-  <p>Capital:${country.capital}</p>
-  <p>Population:${country.population}</p>
-  <p>Languages:${country.languages}</p>
+  <p class="title">Capital:<span class="text">${country.capital}</span></p>
+  <p class="title">Population:<span class="text">${country.population}</span></p>
+  <p class="title">Languages:<span class="text">${lang}</span></p>
 </div>`
     )
     .join('');
   refs.info.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearMarkup() {
+  refs.list.innerHTML = '';
+  refs.info.innerHTML = '';
+}
+
+function error() {
+  Notify.failure('Oops, there is no country with that name');
 }
